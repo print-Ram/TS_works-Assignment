841b91440d7a451939362c87e61aef6b77c8f49a
Sreeram <116187896+print-Ram@users.noreply.github.com>
2023-02-09 20:50:35 +0530

    Update Python Assignment.py

--- Python Assignment.py
+++ Python Assignment.py
@@ -87,3 +87,33 @@ if response.status_code == 200:
 else:
     # If the request was unsuccessful, print an error message
     print("Request failed with status code:", response.status_code)
+    
+    
+    
+import datetime as dt
+import pandas as pd
+import configparser
+import mysql.connector
+import requests
+stockdata = input('\n\t\t\t\t****Select any Company Name****\n')
+ts1 = str(int(dt.datetime(2023,2,1).timestamp()))
+ts2 = str(int(dt.datetime(2023,2,8).timestamp()))
+interval = input('Enter the interval from 1d to 1mo :\n\t daywise = 1d\n\t weekwise = 1wk\n\t monthwise = 1mo\n')
+
+#interval = '1d' , '1wk' ,'1mo'
+events = input('Want to Print a Historical data or Dividened ? \n')
+
+#events = 'history','split'
+Stockurl = 'https://finance.yahoo.com/quote/'+stockdata+'/history?p='+stockdata+''
+
+print('\n\nTo view it Click here :')
+print(Stockurl)
+
+Stock_downurl = 'https://query1.finance.yahoo.com/v7/finance/download/' + stockdata + '?period1='+ ts1 + '&period2=' + ts2 + '&interval=' + interval + '&events'+ events
+company_data = pd.read_csv(Stock_downurl)
+print(company_data)
+
+print('\n\nThe Download link is here :')
+print(Stock_downurl)
+
+print('\n/////Hence you Can change the "date interval" at line no.4 and 5 in code and try/////\n')
